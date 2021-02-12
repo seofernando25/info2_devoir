6c071c974239b798fe01c0778b1a58dce5e5dbfd
Fernando Nogueira <mrfernando2509@gmail.com>
2021-02-11 19:00:12 -0500

    Updated readme file

--- Devoir_1/TestCouple.java
+++ Devoir_1/TestCouple.java
@@ -10,7 +10,6 @@ public class TestCouple {
         c1.display();
         c2.display();
         c3.display();
-
         c3.setP(8);
         System.out.println("Après modification, les éléments de c3 sont: " + c3.getP() + " " + c3.getQ());
         System.out.println("c1 est inferieur a c3: " + c1.compare(c3));
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# info2_devoir
-Repository for ITI 1521 homework
+Nom étudiant : Fernando Berti Cruz Nogueira
+Numéro d’étudiant : 300195829
+
+Nom étudiant : Tyler Blinson
+Numéro d’étudiant : 300169866
+
+Code du cours : ITI1521
+Section Lab: A01
